1118f8f5d60d045da01d8e13063b8a127e3fcc6a
Mark S. <the@wondersmith.dev>
2023-08-31 15:55:41 -0400

    style: bubble up storage and serializer modules

--- perscache/__init__.py
+++ perscache/__init__.py
@@ -1,3 +1,19 @@
-__all__ = ["Cache", "NoCache"]
+"""perscache."""
 
-from .cache import Cache, NoCache
+# Future Imports
+from __future__ import annotations
+
+# Imports From Package Sub-Modules
+from . import (
+    serializers,
+    storage,
+)
+from .cache import (
+    Cache,
+    NoCache,
+)
+
+__all__ = (
+    "Cache",
+    "NoCache",
+)
