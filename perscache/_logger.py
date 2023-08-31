27bc4f96304ad3012a00ea54340979c92ada8e57
Mark S. <the@wondersmith.dev>
2023-08-31 15:57:08 -0400

    feat: add TRACE logging level

--- perscache/_logger.py
+++ perscache/_logger.py
@@ -1,27 +1,57 @@
-from logging import getLogger, DEBUG, NullHandler
-import functools as ft
+"""perscache - logging utilities."""
 
-logger = getLogger("perscache")
-logger.setLevel(DEBUG)
-logger.addHandler(NullHandler())
+# Future Imports
+from __future__ import annotations
 
-debug = logger.debug
+# Standard Library Imports
+import logging
+from functools import wraps
+from typing import (
+    Any,
+    Callable,
+)
 
-MAX_LEN = 200
+__all__ = ("trace",)
+
+MAX_LEN: int = 200
+TRACE_LEVEL: int = 1
+TRACE_NAME: str = "TRACE"
+
+logging.addLevelName(TRACE_LEVEL, TRACE_NAME)
+
+logger = logging.getLogger("perscache")
+logger.setLevel(logging.DEBUG)
+logger.addHandler(logging.NullHandler())
 
 
-def _trim(s):
-    return f"{str(s)[:MAX_LEN]}..." if len(str(s)) > MAX_LEN else s
+def _trim(string: Any) -> str:
+    """Trim the supplied string to be under `MAX_LEN` characters."""
+    if MAX_LEN < len(string := str(string)):
+        string = f"{string[:MAX_LEN]}..."
+
+    return string
 
 
-def trace(f):
-    @ft.wraps(f)
-    def wrapper(*args, **kwargs):
-        debug(
-            f"Entering {_trim(f.__name__)}, args={_trim(args)}, kwargs={_trim(kwargs)}"
+def trace(func: Callable[..., Any]) -> Callable[..., Any]:
+    """Wrap the supplied function with entry and exit logs."""
+
+    @wraps(func)
+    def wrapper(*args: Any, **kwargs: Any):
+        """Log entry and exit from the decorated function."""
+        func_name = _trim(func.__name__)
+
+        logger.log(
+            TRACE_LEVEL,
+            f"Entering {func_name}, args={_trim(args)}, kwargs={_trim(kwargs)}",
         )
-        result = f(*args, **kwargs)
-        debug(f"Exiting {_trim(f.__name__)}, result={_trim(result)}")
+
+        result = func(*args, **kwargs)
+
+        logger.log(
+            TRACE_LEVEL,
+            f"Exiting {func_name}, result={_trim(result)}",
+        )
+
         return result
 
     return wrapper
